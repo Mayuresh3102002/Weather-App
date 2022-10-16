bbb47f6f8c375cd4b1bfba8f0dc631024afef2e2
Mayuresh Gajalwar <mayureshgajalwar3@gmail.com>
2022-10-16 16:05:12 +0530

    First commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f778b92e41msh611305a35446cffp1b0487jsn088190892cf1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "ef8085f044166afb2b08c644bb466e45";
